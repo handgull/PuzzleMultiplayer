91e417245b90de9609521de3e0e3fd0241674366
handgull <gullandrea693@gmail.com>
2020-04-10 13:20:38 +0200

    fix

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	matchescontroller "PuzzleMultiplayer/delivery/websockets"
-	"bufio"
 	"flag"
 	"log"
-	"os"
 )
 
 var (
@@ -15,13 +13,7 @@ var (
 func main() {
 	flag.Parse()
 	// Faccio partire l'api che gestisce i match su websocket
-	go func() {
-		if err := matchescontroller.RunWSMatches("/matches", *wsAddr, 8192, 8192, 4096); err != nil {
-			log.Panic(err)
-		}
-	}()
-
-	// Blocco l'esecuzione in attesa di un input da tastiera (qui in futuro ci sarà la serve dell'api rest)
-	reader := bufio.NewReader(os.Stdin)
-	reader.ReadString('\n')
+	if err := matchescontroller.RunWSMatches("/matches", *wsAddr, 8192, 8192, 4096); err != nil {
+		log.Panic(err)
+	}
 }
