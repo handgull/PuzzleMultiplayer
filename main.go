7fba7ced5fbebc8214aa9f19d1f9057c41099843
handgull <gullandrea693@gmail.com>
2020-04-10 13:44:01 +0200

    fix2

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 var (
-	wsAddr = flag.String("ws", "localhost:9000", "Address for the websocket matches server to listen on")
+	wsAddr = flag.String("ws", ":9000", "Address for the websocket matches server to listen on")
 )
 
 func main() {
 	flag.Parse()
 	// Faccio partire l'api che gestisce i match su websocket
-	if err := matchescontroller.RunWSMatches("/matches", *wsAddr, 8192, 8192, 4096); err != nil {
+	if err := matchescontroller.RunWSMatches("/ws", *wsAddr, 8192, 8192, 4096); err != nil {
 		log.Panic(err)
 	}
 }
